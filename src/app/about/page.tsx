68709a0a8dec52e577d2c4a96f49b88c64e6ced0
Bag Carrier <1375322655@qq.com>
2025-09-08 00:26:04 +0800

    feat: integrate shadcn/ui and style the main layout

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from '@/components/ui/button';
+
 export default function page() {
-  return <div>About Page</div>;
+  return (
+    <>
+      <div>About Page</div>
+      <Button>Click Me</Button>
+    </>
+  );
 }
